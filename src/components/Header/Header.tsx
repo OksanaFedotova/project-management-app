ab94fb91caba6143082e65c03c8283fa55657d9a
DaryaDak <darya.dakuko@gmail.com>
2022-11-11 17:03:49 +0300

    feat: add welcome page styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,10 @@ import { AppBar, Toolbar, Typography, ToggleButtonGroup, ToggleButton } from '@m
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Buttons from '../Buttons';
+import './Header.css';
+import { NavLink, useNavigate } from 'react-router-dom';
+import type { RootState } from '../../store/store';
+import { useSelector } from 'react-redux';
 
 const theme = createTheme({
   palette: {
@@ -12,18 +16,30 @@ const theme = createTheme({
   },
 });
 
-const Header = () => {
+const Header = ({ isSticky }: { isSticky: boolean }) => {
   const [alignment, setAlignment] = React.useState('ru');
 
   const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
     setAlignment(newAlignment);
   };
+
+  const navigator = useNavigate();
+
+  const isAuth = useSelector((state: RootState) => state.auth.user);
+
   return (
-    <header>
-      <AppBar position="static">
-        <Toolbar>
-          <HomeOutlinedIcon />
-          <Typography variant="h6" color="inherit" sx={{ flexGrow: 1, pl: 1 }} component="div">
+    <header className={isSticky ? 'appbar-sticky' : 'appbar'}>
+      <AppBar className={isSticky ? 'appbar-sticky' : 'appbar'}>
+        <Toolbar className="toolbar">
+          <HomeOutlinedIcon className="welcome-page-link" onClick={() => navigator('..')} />
+          <Typography
+            variant="h6"
+            color="inherit"
+            sx={{ flexGrow: 1, pl: 1 }}
+            component="div"
+            className="welcome-page-link"
+            onClick={() => navigator('..')}
+          >
             Главная
           </Typography>
           <ThemeProvider theme={theme}>
@@ -40,8 +56,21 @@ const Header = () => {
               <ToggleButton value="en">EN</ToggleButton>
             </ToggleButtonGroup>
           </ThemeProvider>
-          <Buttons text="Вход" />
-          <Buttons text="Регистрация" />
+          {!isAuth && (
+            <>
+              <NavLink to={'/sign-in'} style={{ color: `inherit`, textDecoration: `none` }}>
+                <Buttons text="Вход" />
+              </NavLink>
+              <NavLink to={'/sign-up'} style={{ color: `inherit`, textDecoration: `none` }}>
+                <Buttons text="Регистрация" />
+              </NavLink>
+            </>
+          )}
+          {isAuth && (
+            <NavLink to={'/welcome'} style={{ color: `inherit`, textDecoration: `none` }}>
+              <Buttons text="На главную" />
+            </NavLink>
+          )}
         </Toolbar>
       </AppBar>
     </header>
