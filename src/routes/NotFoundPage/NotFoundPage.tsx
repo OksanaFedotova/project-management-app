16186e1c108a6f975aaa4a12b99db482e8bae8ba
DaryaDak <darya.dakuko@gmail.com>
2022-11-19 18:26:05 +0300

    feat: add 404 page

--- src/routes/NotFoundPage/NotFoundPage.tsx
+++ src/routes/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Layout from '../../components/Layout';
+import notFoundImage from '../../assets/notFound.jpg';
+import './NotFoundPage.css';
+import { NavLink } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <>
+      <Layout>
+        <div className="not-found-page">
+          <img className="not-found-img" src={notFoundImage} alt="Not Found Page Image" />
+          <NavLink to="/welcome" style={{ color: `inherit`, textDecoration: `none` }}>
+            <Button variant="contained">На главную</Button>
+          </NavLink>
+        </div>
+      </Layout>
+    </>
+  );
 }
