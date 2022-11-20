c6650b0a117f307f6a6b0f25266c4bdb40c7cd69
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-11-21 02:57:30 +0700

    feat: add loading animation on signin-signup-edit

--- src/components/EditProfile/EditProfile.tsx
+++ src/components/EditProfile/EditProfile.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { Container, Box, Typography, TextField, Button } from '@mui/material';
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+  Backdrop,
+} from '@mui/material';
 import { useDeleteUserMutation, useUpdateUserMutation } from 'store/services/userAPI';
 import { ErrorAuth, ISignupRequest } from 'interfaces/IUser';
 import { removeUser, setUpdatedUser } from 'store/reducers/AuthSlice';
@@ -59,113 +67,112 @@ export default function EditProfile() {
           alignItems: 'center',
         }}
       >
-        {isLoadingUpdate || isLoadingDelete ? (
-          <h2 style={{ color: '#000000' }}>Тут будет анимация загрузки с оверлеем</h2>
-        ) : (
-          <>
-            <Typography component="h1" variant="h5">
-              Редактирование профиля
-            </Typography>
-            <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit(onSubmit)}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label={errors.name ? errors.name.message : 'Имя'}
-                type="text"
-                error={!!errors.name}
-                autoComplete="on"
-                {...register('name', {
-                  required: {
-                    value: true,
-                    message: 'Имя обязательно',
-                  },
-                  maxLength: {
-                    value: 25,
-                    message: 'Максимум 25 символов',
-                  },
-                  pattern: {
-                    value: /^[A-Za-z]+$/i,
-                    message: 'Имя должно состоять из латинских букв',
-                  },
-                })}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="login"
-                label={errors.login ? errors.login.message : 'Логин'}
-                type="text"
-                error={!!errors.login}
-                autoComplete="on"
-                {...register('login', {
-                  required: {
-                    value: true,
-                    message: 'Логин обязателен',
-                  },
-                  minLength: {
-                    value: 4,
-                    message: 'Минимум 4 символа',
-                  },
-                  maxLength: {
-                    value: 25,
-                    message: 'Максимум 25 символов',
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9]+$/i,
-                    message: 'Логин из латинских букв или цифр',
-                  },
-                })}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                label={errors.password ? errors.password.message : 'Пароль'}
-                type="password"
-                error={!!errors.password}
-                autoComplete="on"
-                {...register('password', {
-                  required: {
-                    value: true,
-                    message: 'Пароль обязателен',
-                  },
-                  minLength: {
-                    value: 6,
-                    message: 'Минимум 6 символов',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: 'Максимум 20 символов',
-                  },
-                  pattern: {
-                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,}$/i,
-                    message: 'Пароль из латинских букв и цифр',
-                  },
-                })}
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                color="success"
-                sx={{ mt: 3, mb: 2, mr: 4, ml: 2 }}
-              >
-                Редактировать
-              </Button>
-              <Button
-                onClick={deleteProfile}
-                variant="outlined"
-                color="error"
-                sx={{ mt: 3, mb: 2, mr: 2 }}
-              >
-                Удалить профиль
-              </Button>
-            </Box>
-          </>
+        {(isLoadingUpdate || isLoadingDelete) && (
+          <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
+            <CircularProgress color="inherit" size={60} />
+          </Backdrop>
         )}
+        <Typography component="h1" variant="h5">
+          Редактирование профиля
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label={errors.name ? errors.name.message : 'Имя'}
+            type="text"
+            error={!!errors.name}
+            autoComplete="on"
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Имя обязательно',
+              },
+              maxLength: {
+                value: 25,
+                message: 'Максимум 25 символов',
+              },
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: 'Имя должно состоять из латинских букв',
+              },
+            })}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="login"
+            label={errors.login ? errors.login.message : 'Логин'}
+            type="text"
+            error={!!errors.login}
+            autoComplete="on"
+            {...register('login', {
+              required: {
+                value: true,
+                message: 'Логин обязателен',
+              },
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+              maxLength: {
+                value: 25,
+                message: 'Максимум 25 символов',
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9]+$/i,
+                message: 'Логин из латинских букв или цифр',
+              },
+            })}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label={errors.password ? errors.password.message : 'Пароль'}
+            type="password"
+            error={!!errors.password}
+            autoComplete="on"
+            {...register('password', {
+              required: {
+                value: true,
+                message: 'Пароль обязателен',
+              },
+              minLength: {
+                value: 6,
+                message: 'Минимум 6 символов',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Максимум 20 символов',
+              },
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,}$/i,
+                message: 'Пароль из латинских букв и цифр',
+              },
+            })}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="success"
+            sx={{ mt: 3, mb: 2, mr: 4, ml: 2 }}
+          >
+            Редактировать
+          </Button>
+          <Button
+            onClick={deleteProfile}
+            variant="outlined"
+            color="error"
+            sx={{ mt: 3, mb: 2, mr: 2 }}
+          >
+            Удалить профиль
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
