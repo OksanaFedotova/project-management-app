115a70ede7e32a36b199d33b7cd793676f763ddf
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-12-01 00:27:51 +0700

    feat: add username in task

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -65,6 +65,7 @@ export const messages = {
     description_required: 'Описание обязательно',
     description_max_length: 'Максимум 100 символов',
     board_delete_toast: 'Доска удалена',
+    owner: 'Создатель',
   },
   [LOCALES.ENGLISH]: {
     project_title: 'Task manager',
@@ -130,5 +131,6 @@ export const messages = {
     description_required: 'Description required',
     description_max_length: 'Max: 100 symbols',
     board_delete_toast: 'Board deleted',
+    owner: 'Owner',
   },
 };
